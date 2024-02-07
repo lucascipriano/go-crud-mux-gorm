c8b2bac8b4dc325bdcb68686a7a9ec32cfd1085a
Lucas Cipriano <lucasncborges@gmail.com>
2024-02-07 11:21:59 -0300

    errei, fui mlk

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crud/handles"
-	"crud/initializers"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,16 +10,15 @@ import (
 )
 
 func init() {
-	initializers.ConnectDb()
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/user", handles.CreateUser).Methods(http.MethodPost)
 	router.HandleFunc("/user", handles.SearchAllUsers).Methods(http.MethodGet)
-	router.HandleFunc("/user/{uuid}", handles.SearchUser).Methods(http.MethodGet)
-	router.HandleFunc("/user/{uuid}", handles.UpdateUser).Methods(http.MethodPut)
-	router.HandleFunc("/user/{uuid}", handles.DeleteUser).Methods(http.MethodDelete)
+	router.HandleFunc("/user/{id}", handles.SearchUser).Methods(http.MethodGet)
+	router.HandleFunc("/user/{id}", handles.UpdateUser).Methods(http.MethodPut)
+	router.HandleFunc("/user/{id}", handles.DeleteUser).Methods(http.MethodDelete)
 
 	fmt.Println("Api rodando na porta 2121")
 	log.Fatal(http.ListenAndServe(":2121", router))
